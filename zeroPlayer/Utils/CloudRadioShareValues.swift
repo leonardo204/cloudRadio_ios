fe0efe12efab3bf264906bfb069c20dd9e9a2016
leonardo204 <zerolive7@gmail.com>
2022-04-19 15:55:27 +0900

    - 버전 변경(v1.5)

--- zeroPlayer/Utils/CloudRadioShareValues.swift
+++ zeroPlayer/Utils/CloudRadioShareValues.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CloudRadioShareValues {
     // Version
-    static var versionString = "v1.4"
+    static var versionString = "v1.5"
     
     // hidden count
     static var IsUnlockedFeature = false
